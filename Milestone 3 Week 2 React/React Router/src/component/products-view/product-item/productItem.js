724562e19b2189d156e7fb227fbe167a9b212c84
PrakharPriyesh <1604294@kiit.ac.in>
2020-03-30 17:48:28 +0530

    Converted function to ES6 | destructured props

--- Milestone 3 Week 2 React/React Router/src/component/products-view/product-item/productItem.js
+++ Milestone 3 Week 2 React/React Router/src/component/products-view/product-item/productItem.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './productItem.css'
 
-function ProductCard({element,sellerId,addItemToCartHandler,uniqueKey}) {
+const ProductCard = ({ element, sellerId, addItemToCartHandler, uniqueKey }) => {
+    const { img, name, stock, price } = element
     return (
         <div className="product-card">
             <div className="product-body">
-                <img className="product-img" src={element.img} alt={element.img} onError={(e) => { e.target.onerror = null; e.target.src = "https://images.vexels.com/media/users/3/148920/isolated/preview/a8016baffbce8083fcc0313f2c904e6e-red-office-chair-clipart-by-vexels.png" }} />
-                <div className="product-name">{element.name}</div>
+                <img className="product-img" src={img} alt={img} onError={(e) => { e.target.onerror = null; e.target.src = "https://images.vexels.com/media/users/3/148920/isolated/preview/a8016baffbce8083fcc0313f2c904e6e-red-office-chair-clipart-by-vexels.png" }} />
+                <div className="product-name">{name}</div>
                 <div className="product-text">
-                    Rs. {element.price + " "} <br />
-                    <font className="product-stock">Stock: {element.stock}</font>
+                    Rs. {price + " "} <br />
+                    <font className="product-stock">Stock: {stock}</font>
                 </div>
                 <button className="product-button" onClick={addItemToCartHandler} id={uniqueKey} value={sellerId}>Add to Cart</button>
             </div>
