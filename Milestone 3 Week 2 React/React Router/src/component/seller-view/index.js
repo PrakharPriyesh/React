0175743e86fef6c968c2c6face6260231098ee1e
PrakharPriyesh <1604294@kiit.ac.in>
2020-03-30 18:14:59 +0530

    Added file sellerPresenter

--- Milestone 3 Week 2 React/React Router/src/component/seller-view/index.js
+++ Milestone 3 Week 2 React/React Router/src/component/seller-view/index.js
@@ -1,5 +1,6 @@
 import SellerView from './sellerView'
 import SellerCard from './seller-card/sellerCard'
 import SellerList from './seller-list/sellerList'
+import SellerPresenter from './sellerPresenter'
 
-export {SellerView, SellerCard, SellerList}
+export {SellerView, SellerCard, SellerList, SellerPresenter}
---
+++ Milestone 3 Week 2 React/React Router/src/component/seller-view/sellerPresenter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { CartView } from '../cart-view';
+import { SellerList } from './';
+import { ProductsView } from '../products-view';
+import Navbar from '../navbar';
+import './sellerView.css';
+
+const SellerPresenter = ({search, updateSearch, products, addItemToCartHandler, totalCartValue, cartProducts, removeItemFromCart}) => {
+    return (
+        <>
+            <Navbar
+                search={search}
+                updateSearch={updateSearch}
+            />
+            <div className="grid-container">
+                <div className="grid-item item1">
+                    <SellerList />
+                    <ProductsView
+                        products={products}
+                        addItemToCartHandler={addItemToCartHandler}
+                        search={search}
+                    />
+                </div>
+                <div className="grid-item item2">
+                    <CartView
+                        totalCartValue={totalCartValue}
+                        cartProducts={cartProducts}
+                        removeItemFromCart={removeItemFromCart}
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default SellerPresenter;
