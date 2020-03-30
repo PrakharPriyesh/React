a2023ea4547430c4e277b157654ef02be08f0ca6
PrakharPriyesh <1604294@kiit.ac.in>
2020-03-30 15:53:07 +0530

    added icon property

--- Milestone 3 Week 2 React/React Router/src/utils/sellerDetails.js
+++ Milestone 3 Week 2 React/React Router/src/utils/sellerDetails.js
@@ -1,7 +1,7 @@
 const sellerDetails = {
-    "s1" : {name: "Seller One", rating: "5", address: "India", contact: "9876543210"},
-    "s2" : {name: "Seller One", rating: "5", address: "India", contact: "9876543210"},
-    "s3" : {name: "Seller One", rating: "5", address: "India", contact: "9876543210"},
+    "s1" : {name: "Seller One", rating: "5", address: "India", contact: "9876543210", icon: "https://images-na.ssl-images-amazon.com/images/I/41FaaziVejL._SY355_.png"},
+    "s2" : {name: "Seller One", rating: "5", address: "India", contact: "9876543210", icon: "https://www.searchpng.com/wp-content/uploads/2019/01/Myntra-logo-png-icon-715x715.png"},
+    "s3" : {name: "Seller One", rating: "5", address: "India", contact: "9876543210", icon: "https://saledunia.com/wp-content/uploads/2019/08/flipkart_logo_detail_icon.jpg"},
     // "s4" : {name: "Seller One", rating: "5", address: "India", contact: "9876543210"},
     // "s5" : {name: "Seller One", rating: "5", address: "India", contact: "9876543210"},
     // "s6" : {name: "Seller One", rating: "5", address: "India", contact: "9876543210"},
