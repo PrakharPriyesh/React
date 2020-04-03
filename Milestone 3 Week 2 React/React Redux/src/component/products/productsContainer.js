a1470eea9f0e204e9fc83d3fc97840ed659d2a8d
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-03 11:13:15 +0530

    restructured productsContainer.js to improve code

--- Milestone 3 Week 2 React/React Redux/src/component/products/productsContainer.js
+++ Milestone 3 Week 2 React/React Redux/src/component/products/productsContainer.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { ProductItem } from '.'
 
 const ProductsContainer = ({ products, addItemToCartHandler, search }) => {
-    let href = window.location.href.split("/")
-    let sellerId = href[href.length - 1]
-    if (window.location.href === ('http://localhost:3000/sellers/' + sellerId)) {
+
+    const getFilteredProducts = (products, search) => {
+        let href = window.location.href.split("/")
+        let sellerId = href[href.length - 1]
         products = Object.assign({}, ...products[sellerId]);
-        const filteredProducts = (
+        let filteredProducts = (
             Object.keys(products).map((key) => {
-                if (products[key].name.toLowerCase().indexOf(search) !== -1) {
+                if (products[key].name.toLowerCase().indexOf(search.toLowerCase()) !== -1) {
                     let data = {};
                     data[key] = products[key];
                     return data;
@@ -16,35 +17,30 @@ const ProductsContainer = ({ products, addItemToCartHandler, search }) => {
                 return null;
             })
         )
-        let items = null
-        products = Object.assign({}, ...filteredProducts);
-        items = (
-            Object.keys(products).map((keys) => {
-                const element = products[keys]
-                return (
-                    <ProductItem
-                        key={keys}
-                        uniqueKey={keys}
-                        element={element}
-                        sellerId={sellerId}
-                        addItemToCartHandler={addItemToCartHandler}
-                    />
-                )
-            })
-        )
-
-        return (
-            <>
-                {items}
-            </>
-        );
-
+        filteredProducts = Object.assign({}, ...filteredProducts);
+        return { filteredProducts, sellerId }
     }
 
-    else {
-        return <> </>
-    }
+    const { filteredProducts, sellerId } = getFilteredProducts(products, search);
 
+    return (
+        <>
+            {
+                Object.keys(filteredProducts).map((keys) => {
+                    const element = filteredProducts[keys]
+                    return (
+                        <ProductItem
+                            key={keys}
+                            uniqueKey={keys}
+                            element={element}
+                            sellerId={sellerId}
+                            addItemToCartHandler={addItemToCartHandler}
+                        />
+                    )
+                })
+            }
+        </>
+    );
 }
 
 export default ProductsContainer;
