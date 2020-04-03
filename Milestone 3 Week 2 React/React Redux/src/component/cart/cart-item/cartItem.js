80ec52fad418c2578d9d6e9f3488a41b7b37d1c8
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-03 11:12:03 +0530

    restructured cartItem.js

--- Milestone 3 Week 2 React/React Redux/src/component/cart/cart-item/cartItem.js
+++ Milestone 3 Week 2 React/React Redux/src/component/cart/cart-item/cartItem.js
@@ -2,29 +2,25 @@ import React from 'react';
 import './cartItem.css'
 
 const CartItem = ({ cartProducts, removeItemFromCart }) => {
-    let items = null
-    items = (
-        Object.keys(cartProducts).map((key) => {
-            const {img, name,price,count,seller} = cartProducts[key]
-            return (
-                // Did not create another component as css were different will learn to use relative dimensions
-                <div className="cart-product-card" key={key}>
-                    <div className="cart-product-body">
-                        <img className="cart-product-img" src={img} alt={img} onError={(e) => { e.target.onerror = null; e.target.src = "https://images.vexels.com/media/users/3/148920/isolated/preview/a8016baffbce8083fcc0313f2c904e6e-red-office-chair-clipart-by-vexels.png" }} />
-                        <div className="cart-product-name">{name}</div>
-                        <div className="cart-product-text">
-                            <b>Rs. {price} X {count}</b>
-                        </div>
-                        <button className="cart-product-button" id={key} onClick={removeItemFromCart} value={seller}>Remove</button>
-                    </div>
-                </div>
-            )
-        })
-    )
-
     return (
         <>
-            {items}
+            {
+                Object.keys(cartProducts).map((key) => {
+                    const { img, name, price, count, seller } = cartProducts[key]
+                    return (
+                        <div className="cart-product-card" key={key}>
+                            <div className="cart-product-body">
+                                <img className="cart-product-img" src={img} alt={img} onError={(e) => { e.target.onerror = null; e.target.src = "https://images.vexels.com/media/users/3/148920/isolated/preview/a8016baffbce8083fcc0313f2c904e6e-red-office-chair-clipart-by-vexels.png" }} />
+                                <div className="cart-product-name">{name}</div>
+                                <div className="cart-product-text">
+                                    <b>Rs. {price} X {count}</b>
+                                </div>
+                                <button className="cart-product-button" id={key} onClick={removeItemFromCart} value={seller}>Remove</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </>
     );
 }
