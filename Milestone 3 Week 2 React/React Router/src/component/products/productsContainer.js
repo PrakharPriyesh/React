47bd64eeda09b71e6b31717214b4daf05d7f5e43
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-03 10:07:27 +0530

    restructured ProductsContainer.js to improve code

--- Milestone 3 Week 2 React/React Router/src/component/products/productsContainer.js
+++ Milestone 3 Week 2 React/React Router/src/component/products/productsContainer.js
@@ -2,48 +2,48 @@ import React from 'react';
 import { ProductItem } from '.'
 
 const ProductsContainer = ({ products, addItemToCartHandler, search }) => {
-    let href = window.location.href.split("/")
-    let sellerId = href[href.length - 1]
-    if (window.location.href === ('http://localhost:3000/sellers/' + sellerId)) {
-        products = Object.assign({}, ...products[sellerId]);
-        const filteredProducts = (
-            Object.keys(products).map((key) => {
-                if (products[key].name.toLowerCase().indexOf(search) !== -1) {
-                    let data = {};
-                    data[key] = products[key];
-                    return data;
-                }
-                return null;
-            })
-        )
-        let items = null
-        products = Object.assign({}, ...filteredProducts);
-        items = (
-            Object.keys(products).map((keys) => {
-                const element = products[keys]
-                return (
-                    <ProductItem
-                        key={keys}
-                        uniqueKey={keys}
-                        element={element}
-                        sellerId={sellerId}
-                        addItemToCartHandler={addItemToCartHandler}
-                    />
-                )
-            })
-        )
 
-        return (
-            <>
-                {items}
-            </>
-        );
+    const getFilteredProducts = (products, search) => {
+        let href = window.location.href.split("/")
+        let sellerId = href[href.length - 1]
 
+        if (window.location.href === ('http://localhost:3000/sellers/' + sellerId)) {
+            products = Object.assign({}, ...products[sellerId]);
+            let filteredProducts = (
+                Object.keys(products).map((key) => {
+                    if (products[key].name.toLowerCase().indexOf(search.toLowerCase()) !== -1) {
+                        let data = {};
+                        data[key] = products[key];
+                        return data;
+                    }
+                    return null;
+                })
+            )
+            filteredProducts = Object.assign({}, ...filteredProducts);
+            return {filteredProducts, sellerId}
+        }
     }
 
-    else {
-        return <> </>
-    }
+    const {filteredProducts, sellerId} = getFilteredProducts(products, search);
+
+    return (
+        <>
+            {
+                Object.keys(filteredProducts).map((keys) => {
+                    const element = filteredProducts[keys]
+                    return (
+                        <ProductItem
+                            key={keys}
+                            uniqueKey={keys}
+                            element={element}
+                            sellerId={sellerId}
+                            addItemToCartHandler={addItemToCartHandler}
+                        />
+                    )
+                })
+            }
+        </>
+    );
 
 }
 
