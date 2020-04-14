60b6dfe594d6fceca9fbfb6529c61a4f46bc1bb4
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-14 17:41:31 +0530

    added import for navbar in dashboard

--- FED/assignment/src/component/dashboard/dashboard.js
+++ FED/assignment/src/component/dashboard/dashboard.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
+
 import './dashboard.css';
+import Navbar from './../navbar/navbar';
 
 function Dashboard(props) {
     return (
-        <div className="dashboard-container">
+        <>
+            <Navbar />
+            <div className="dashboard-container">
                 <div className="item1">1</div>
                 <div className="item2">2</div>
                 <div className="item3">3</div>
                 <div className="item4">4</div>
                 <div className="item5">5</div>
-        </div>
+            </div>
+        </>
     );
 }
 
