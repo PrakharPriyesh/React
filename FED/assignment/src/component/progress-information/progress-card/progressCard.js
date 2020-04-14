dbde0c19239183a10081d94d2264c917e55b74e3
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-14 20:45:52 +0530

    Added component progress card

---
+++ FED/assignment/src/component/progress-information/progress-card/progressCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './progressCard.css'
+
+function ProgressCard(props) {
+    return (
+        <div className="progress-card">
+            
+        </div>
+    );
+}
+
+export default ProgressCard;
