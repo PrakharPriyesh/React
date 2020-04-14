89e5a7998a84b45e2ca5856d2c5be70db7dd1f6b
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-14 17:40:48 +0530

    Created navbar component

---
+++ FED/assignment/src/component/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './navbar.css'
+
+function Navbar(props) {
+    return (
+        <div className="navbar">
+            fsfds
+        </div>
+    );
+}
+
+export default Navbar;
