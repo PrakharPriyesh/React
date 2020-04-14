13bb93cb7f3147e063aa3ccdd805e699ffc06e92
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-14 19:48:24 +0530

    Added buttons in sidebar

--- FED/assignment/src/component/sidebar/sidebar.js
+++ FED/assignment/src/component/sidebar/sidebar.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import homeIcon from './../../asset/home.png'
+import stackIcon from './../../asset/stack.png'
+import './sidebar.css'
+
 function Sidebar(props) {
     return (
         <div className="sidebar">
-            
+        <div className="sidebar-dashboard">
+            <button>Dashboard</button>
+            <img src={homeIcon} alt={homeIcon} />
+        </div>
+        <div className="sidebar-catalog">
+            <button>Product Catalog</button>
+            <img src={stackIcon} alt={stackIcon} />
+        </div>
         </div>
     );
 }
