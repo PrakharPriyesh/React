3ed0bd40b716043d3ddbf7a8dff2c9515eacee98
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-14 18:38:22 +0530

    added import for sidebar

--- FED/assignment/src/component/dashboard/dashboard.js
+++ FED/assignment/src/component/dashboard/dashboard.js
@@ -3,13 +3,16 @@ import React from 'react';
 
 import './dashboard.css';
 import Navbar from './../navbar/navbar';
+import Sidebar from './../sidebar/sidebar';
 
 function Dashboard(props) {
     return (
         <>
             <Navbar />
             <div className="dashboard-container">
-                <div className="item1">1</div>
+                <div className="item1">
+                    <Sidebar />
+                </div>
                 <div className="item2">2</div>
                 <div className="item3">3</div>
                 <div className="item4">4</div>
