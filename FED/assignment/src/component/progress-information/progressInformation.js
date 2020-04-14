03bd6a65f4940a457c2c93d811f7023675df5746
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-14 20:14:35 +0530

    created component progress information

---
+++ FED/assignment/src/component/progress-information/progressInformation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProgressInformation(props) {
+    return (
+        <div className="progress-information-container">
+            
+        </div>
+    );
+}
+
+export default ProgressInformation;
