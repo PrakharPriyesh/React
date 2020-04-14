9b0a96274b116579461d5370bd60346bfbafc62e
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-14 13:32:56 +0530

    Changes made in for view in smaller screen

--- FED/assignment/src/component/login/login.js
+++ FED/assignment/src/component/login/login.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import './login.css'
+//import PropTypes from 'prop-types';
+import './login.css';
+import rpLogo from './../../asset/rp-logo.png';
 
 // Login.propTypes = {
-    
+
 // };
 
 function Login(props) {
     return (
         <div className="login">
-            asdlfaldsjfsalkdjflkasfjdsakljsdlkfj
+            <img className="rp-logo" src={rpLogo} alt={rpLogo}></img>
+            <div className="login-heading">
+                WELCOME TO REDPANDA UI HTML - CSS BASIC FED TEMPLATE
+            </div>
+            <div className="login-description">
+                The Red Pandas is a team of paragmatic engineers who care deeplu about the quality of their products and the success of their partners. Over the years, we have mastered best practice in building software products from concept to scale and we continue to learn emerging technologies.
+            </div>
+            <div className="login-card">
+                <div className="login-card-heading">
+                    PLEASE LOG IN
+                </div>
+                <div className="login-form">
+                    <div className="login-form-username">
+                        Username <br/>
+                        <input type="text" placeholder="Email or Username"></input>
+                    </div>
+                    <div className="login-form-password">
+                        Password <br/>
+                        <input type="password" placeholder="Password"></input>
+                    </div>
+                    <div className="login-form-rememberme">
+                        <div>Remember Me</div>
+                        <input type="checkbox"></input>
+                    </div>
+                    <div className="login-form-button">
+                        <button>Sign In</button>
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+            <font>&copy; 2020 RedPanda. All Rights Reserved</font>
+            </div>
         </div>
     );
 }
