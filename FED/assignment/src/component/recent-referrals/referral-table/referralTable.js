cc4c2066d6602ec195a88a139d64ae045f61de7a
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-15 13:40:39 +0530

    bug fixes for console errors in table

--- FED/assignment/src/component/recent-referrals/referral-table/referralTable.js
+++ FED/assignment/src/component/recent-referrals/referral-table/referralTable.js
@@ -6,38 +6,40 @@ function ReferralTable(props) {
     return (
         <div className="referral-table">
             <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Position</th>
-                    <th>Age</th>
-                    <th>Office</th>
-                    <th>Date</th>
-                    <th>Salary</th>
-                </tr>
-                <tr>
-                    <td>Damon</td>
-                    <td>5516 Adolfo Green</td>
-                    <td>18</td>
-                    <td>Littelhaven</td>
-                    <td>2014/06/13</td>
-                    <td>553.563</td>
-                </tr>
-                <tr>
-                    <td>Damon</td>
-                    <td>5516 Adolfo Green</td>
-                    <td>18</td>
-                    <td>Littelhaven</td>
-                    <td>2014/06/13</td>
-                    <td>553.563</td>
-                </tr>
-                <tr>
-                    <td>Damon</td>
-                    <td>5516 Adolfo Green</td>
-                    <td>18</td>
-                    <td>Littelhaven</td>
-                    <td>2014/06/13</td>
-                    <td>553.563</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Age</th>
+                        <th>Office</th>
+                        <th>Date</th>
+                        <th>Salary</th>
+                    </tr>
+                    <tr>
+                        <td>Damon</td>
+                        <td>5516 Adolfo Green</td>
+                        <td>18</td>
+                        <td>Littelhaven</td>
+                        <td>2014/06/13</td>
+                        <td>553.563</td>
+                    </tr>
+                    <tr>
+                        <td>Damon</td>
+                        <td>5516 Adolfo Green</td>
+                        <td>18</td>
+                        <td>Littelhaven</td>
+                        <td>2014/06/13</td>
+                        <td>553.563</td>
+                    </tr>
+                    <tr>
+                        <td>Damon</td>
+                        <td>5516 Adolfo Green</td>
+                        <td>18</td>
+                        <td>Littelhaven</td>
+                        <td>2014/06/13</td>
+                        <td>553.563</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     );
