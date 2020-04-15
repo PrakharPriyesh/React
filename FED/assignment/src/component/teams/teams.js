d68e38dbf57130af29bd504ab119947d21ec65e0
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-15 17:25:54 +0530

    renamed scss for teams component

--- FED/assignment/src/component/teams/teams.js
+++ FED/assignment/src/component/teams/teams.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './teams.css'
+
 function Teams(props) {
     return (
         <div className="teams">
-            <div className="teams-heading">MY TEAMS</div>
+            <div className="teams-heading">YOUR TEAMS</div>
             
         </div>
     );
