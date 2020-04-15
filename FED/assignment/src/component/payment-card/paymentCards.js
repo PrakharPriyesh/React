a2dcb6318a0c2af40a32c105ada7611c9ef404db
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-15 14:39:10 +0530

    created new component payment cards

---
+++ FED/assignment/src/component/payment-card/paymentCards.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PaymentCards(props) {
+    return (
+        <div className="payment-cards">
+            <div className="payment-cards-heading">
+                YOUR CARDS
+            </div>
+            <div className="payment-cards-container">
+                
+            </div>            
+        </div>
+    );
+}
+
+export default PaymentCards;
