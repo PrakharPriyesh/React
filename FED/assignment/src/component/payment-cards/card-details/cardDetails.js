d0a0870f6fe0a1496705b9c87cdb6884738a5857
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-15 16:21:49 +0530

    Added elements to payment card - card details | UI improvement

--- FED/assignment/src/component/payment-cards/card-details/cardDetails.js
+++ FED/assignment/src/component/payment-cards/card-details/cardDetails.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import cardIcon from './../../../asset/bookmark.png';
 import './cardDetails.css';
 
 function CardDetails(props) {
     return (
         <div className="card-details">
-            
+            <img src={cardIcon} alt={cardIcon} />
+            <div className="card-details-card-amount">&#36;189,829.55</div>
+            <div className="card-details-card-name">David Beckham</div>
+            <div className="card-details-card-number">9820-XXX-2341-8942</div>
+            <div className="card-details-card-company">MASTERCARD</div>
+            <div className="card-details-card-expiry">Expires on 03/22</div>
         </div>
     );
 }
