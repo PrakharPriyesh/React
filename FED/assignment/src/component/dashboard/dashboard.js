4f9fc982e9cedab779e84dd6108f3072849717b1
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-15 12:05:33 +0530

    Created new component recent referrals and added import in dashboard

--- FED/assignment/src/component/dashboard/dashboard.js
+++ FED/assignment/src/component/dashboard/dashboard.js
@@ -5,6 +5,7 @@ import './dashboard.css';
 import Navbar from './../navbar/navbar';
 import Sidebar from './../sidebar/sidebar';
 import ProcessingInformation from './../progress-information/progressInformation';
+import RecentReferrals from './../recent-referrals/recentReferrals';
 
 
 function Dashboard(props) {
@@ -18,7 +19,9 @@ function Dashboard(props) {
                 <div className="item2">
                     <ProcessingInformation />
                 </div>
-                <div className="item3">3</div>
+                <div className="item3">
+                    <RecentReferrals />
+                </div>
                 <div className="item4">4</div>
                 <div className="item5">5</div>
             </div>
---
+++ FED/assignment/src/component/recent-referrals/recentReferrals.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './recentReferrals.css';
+
+function RecentReferrals(props) {
+    return (
+        <div className="recent-referrals">
+            
+        </div>
+    );
+}
+
+export default RecentReferrals;
