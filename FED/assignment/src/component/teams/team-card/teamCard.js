bb909b1d949b75da71c0d383f58bee434876d34f
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-15 17:14:09 +0530

    Created new component Teams and Team-Card

---
+++ FED/assignment/src/component/teams/team-card/teamCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TeamCard(props) {
+    return (
+        <div className="team-card">
+            
+        </div>
+    );
+}
+
+export default TeamCard;
---
+++ FED/assignment/src/component/teams/teams.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Teams(props) {
+    return (
+        <div className="teams">
+            <div className="teams-heading">MY TEAMS</div>
+            
+        </div>
+    );
+}
+
+export default Teams;
