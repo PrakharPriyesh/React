1bd164f3b62b3c53dcb2f74aea7fe14736b71760
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-15 11:03:58 +0530

    Added elements in progress card | UI improvements

--- FED/assignment/src/component/progress-information/progress-card/progressCard.js
+++ FED/assignment/src/component/progress-information/progress-card/progressCard.js
@@ -3,9 +3,30 @@ import React from 'react';
 import './progressCard.css'
 
 function ProgressCard(props) {
+    
     return (
         <div className="progress-card">
-            
+            <div className="progress-card-heading">
+                Total Profit
+            </div>
+            <div className="progress-card-details">
+                Profit
+            </div>
+            <div className="progress-card-value">
+                180M
+            </div>
+            <div className="progress-card-progress-bar">
+                <div className="progress-card-progress" style={{width:"65%", height:"100%",backgroundColor:"blue"}}>
+                </div>
+            </div>
+            <div className="progress-card-info">
+                <div className="progress-card-change">
+                    Change
+                </div>
+                <div className="progress-card-percentage">
+                    65%
+                </div>
+            </div>
         </div>
     );
 }
