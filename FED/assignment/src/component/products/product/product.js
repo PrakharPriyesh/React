0b0a009110cef053a6989e01df0077f3ee1bfd3b
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-16 15:43:34 +0530

    created new product component for product cards

---
+++ FED/assignment/src/component/products/product/product.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './product.css'
+
+function Product(props) {
+    return (
+        <div className="product-card">
+            
+        </div>
+    );
+}
+
+export default Product;
--- FED/assignment/src/component/products/products.js
+++ FED/assignment/src/component/products/products.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import Product from './product/product'
 import './products.css';
 
 function Products(props) {
     return (
         <div className="products-container">
-            
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
         </div>
     );
 }
