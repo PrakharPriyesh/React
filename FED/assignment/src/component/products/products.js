435a7b3f8e334c57be6d39df4f784699d64f3fe2
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-16 19:06:40 +0530

    Added heading in product and minor UI bug fixes

--- FED/assignment/src/component/products/products.js
+++ FED/assignment/src/component/products/products.js
@@ -5,17 +5,22 @@ import './products.css';
 
 function Products(props) {
     return (
-        <div className="products-container">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+        <div className="products">
+            <div className="products-heading">
+                <font>PRODUCT CATALOG</font>
+            </div>
+            <div className="products-container">
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+            </div>
         </div>
     );
 }
