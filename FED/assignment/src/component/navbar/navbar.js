78e40b6165f7269272178c94132b8e3167086217
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-16 23:33:27 +0530

    Removed logo from navbar | added media query

--- FED/assignment/src/component/navbar/navbar.js
+++ FED/assignment/src/component/navbar/navbar.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-import rpLogo from './../../asset/rp-logo.png';
+import menuIcon from './../../asset/menu.png';
 import profilePhoto from './../../asset/avatar_one.jpg'
 import './navbar.css';
 
 function Navbar(props) {
     return (
         <div className="navbar">
-            <div className="navbar-logo-container">
-                <img src={rpLogo} alt={rpLogo}/>
-            </div>
+            <img className="navbar-icon-img" src={menuIcon} alt={menuIcon} />
             <div className="navbar-profile-container">
                 <img src={profilePhoto} alt={profilePhoto} />
-               <font>&#9660;</font>
+                <font>&#9660;</font>
             </div>
         </div>
     );
