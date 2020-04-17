ee4af1d85f3e97d8b5ab416809fb93850fe8722e
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-17 20:20:10 +0530

    updated height for products | UI improvements

--- FED/assignment/src/App.js
+++ FED/assignment/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 //import Login from './component/login/login';
-import Dashboard from './component/dashboard/dashboard';
-//import Catalog from './component/catalog/catalog'
+//import Dashboard from './component/dashboard/dashboard';
+import Catalog from './component/catalog/catalog'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Catalog /> */}
-      <Dashboard />
+      <Catalog />
+      {/* <Dashboard /> */}
     </div>
   );
 }
