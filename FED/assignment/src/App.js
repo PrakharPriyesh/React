a9b9ff3c8bdd164ae8d768429fcd504699cd3f49
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-18 10:51:56 +0530

    Added react router in app.js

--- FED/assignment/src/App.js
+++ FED/assignment/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-//import Login from './component/login/login';
-//import Dashboard from './component/dashboard/dashboard';
+
+import Login from './component/login/login';
+import Dashboard from './component/dashboard/dashboard';
 import Catalog from './component/catalog/catalog'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Catalog />
-      {/* <Dashboard /> */}
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route path="/catalog">
+            <Catalog />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
+    </div >
   );
 }
 
