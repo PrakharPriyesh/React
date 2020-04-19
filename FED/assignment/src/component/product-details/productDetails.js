3ad30609898e42c7e358b4e71ef71d42d73c44e6
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-19 10:13:55 +0530

    Created product-details component

---
+++ FED/assignment/src/component/product-details/productDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductDetails(props) {
+    return (
+        <div className="product-details">
+            
+        </div>
+    );
+}
+
+export default ProductDetails;
