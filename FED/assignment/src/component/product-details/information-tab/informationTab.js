e296764d42c6f346c80de718682fc3bcfd7d5390
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-21 12:38:10 +0530

    UI improvements | added tab view

--- FED/assignment/src/component/product-details/information-tab/informationTab.js
+++ FED/assignment/src/component/product-details/information-tab/informationTab.js
@@ -3,28 +3,65 @@ import React from 'react';
 import './informationTab.css';
 
 const InformationTab = (props) => {
+
+
+    const onclickHandler = (event) => {
+        const description = document.getElementsByClassName('information-tab-description')[0];
+        const information = document.getElementsByClassName('information-tab-information')[0];
+        const descriptionButton = document.getElementById('information-tab-button1');
+        const informationButton = document.getElementById('information-tab-button2');
+        
+        if (event.target.id == 'information-tab-button1') {
+            description.style.display = "none";
+            information.style.display = "initial";
+            descriptionButton.style.borderBottom = "3px solid dodgerblue";
+            informationButton.style.borderBottom = "none";
+        }
+        else {
+            description.style.display = "initial";
+            information.style.display = "none";
+            informationButton.style.borderBottom = "3px solid dodgerblue";
+            descriptionButton.style.borderBottom = "none";
+        }
+    }
+
     return (
         <div className="information-tab">
             <div className="information-tab-buttons">
-                <button id="information-tab-button1">Information</button>
-                <button id="information-tab-button2">Description</button>
+                <button id="information-tab-button1" onClick={onclickHandler}>Information</button>
+                <button id="information-tab-button2" onClick={onclickHandler}>Description</button>
             </div>
-            <div id="information-tab-description-1">
-                <ul className="information-tab-list1">
+            <div className="information-tab-information">
+                <ul className="information-tab-information-list">
                     <li>
-                        <font id="information-tab-list-heading"></font>
-                        <font id="information-tab-list-body" ></font>
+                        <div id="information-tab-list-heading">Information Lists</div>
+                        <div id="information-tab-list-body" >Information lists is perfect for defining Information.</div>
                     </li>
-                    <li><font id="information-tab-list-heading"></font>
-                        <font id="information-tab-list-body" ></font>
+                    <li>
+                        <div id="information-tab-list-heading">Information Lists</div>
+                        <div id="information-tab-list-body" >Information lists is perfect for defining Information.</div>
                     </li>
-                    <li><font id="information-tab-list-heading"></font>
-                        <font id="information-tab-list-body" ></font>
+                    <li>
+                        <div id="information-tab-list-heading">Information Lists</div>
+                        <div id="information-tab-list-body" >Information lists is perfect for defining Information.</div>
                     </li>
                 </ul>
             </div>
-            <div id="information-tab-description-2">
-
+            <div className="information-tab-description">
+                <ul className="information-tab-description-list">
+                    <li>
+                        <div id="information-tab-list-heading">Description Lists</div>
+                        <div id="information-tab-list-body" >Description lists is perfect for defining description.</div>
+                    </li>
+                    <li>
+                        <div id="information-tab-list-heading">Description Lists</div>
+                        <div id="information-tab-list-body" >Description lists is perfect for defining description.</div>
+                    </li>
+                    <li>
+                        <div id="information-tab-list-heading">Description Lists</div>
+                        <div id="information-tab-list-body" >Description lists is perfect for defining description.</div>
+                    </li>
+                </ul>
             </div>
         </div>
     );
