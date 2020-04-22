5cf82872e9f0952b7d57147637b290335c2ea07a
PrakharPriyesh <1604294@kiit.ac.in>
2020-04-22 21:23:56 +0530

    Added close button function to modal

--- FED/assignment/src/component/product-details/modal/modal.js
+++ FED/assignment/src/component/product-details/modal/modal.js
@@ -3,8 +3,13 @@ import React from 'react';
 import './modal.scss';
 
 const Modal = (props) => {
+
+    const closeModal = () => {
+        document.getElementById('modal-view').style.display ="none";
+    }
+
     return (
-        <div className="modal">
+        <div id="modal-view" className="modal">
             <div className="modal-card">
                 <div className="modal-card-heading">
                     Flipkart
@@ -13,7 +18,7 @@ const Modal = (props) => {
                     Be it a manufacturer, vendor or supplier, simply sell your products online on Flipkart and become a top e-commerce player with minimum investment. Through a team of experts offering exclusive seller workshops, training, and seller support, Flipkart focuses on empowering sellers across India.Be it a manufacturer, vendor or supplier, simply sell your products online on Flipkart and become a top e-commerce player with minimum investment. Through a team of experts offering exclusive seller workshops, training, and seller support, Flipkart focuses on empowering sellers across India.
                 </div>
                 <div className="modal-card-button">
-                    <button>Close</button>
+                    <button onClick={closeModal}>Close</button>
                 </div>
             </div>
         </div>
